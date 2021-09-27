769edc65229f0cf599f1cab0423d482f5e6340e8
Pedro A Osuna <posuna@cameo.com>
2021-09-27 11:32:57 -0700

    Expose class extension method
    Also update package names for forked npm packages

--- detox/ios/Detox/Utilities/UIWindow+DetoxUtils.h
+++ detox/ios/Detox/Utilities/UIWindow+DetoxUtils.h
@@ -23,6 +23,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 + (NSArray<UIWindow*>*)dtx_allWindows;
 + (NSArray<UIWindow*>*)dtx_allWindowsForScene:(nullable UIWindowScene*)scene;
++ (id)dtx_keyWindowScene;
 + (void)dtx_enumerateAllWindowsUsingBlock:(void (NS_NOESCAPE ^)(UIWindow* obj, NSUInteger idx, BOOL *stop))block;
 + (void)dtx_enumerateKeyWindowSceneWindowsUsingBlock:(void (NS_NOESCAPE ^)(UIWindow* obj, NSUInteger idx, BOOL *stop))block;
 + (void)dtx_enumerateWindowsInScene:(nullable UIWindowScene*)scene usingBlock:(void (NS_NOESCAPE ^)(UIWindow* obj, NSUInteger idx, BOOL *stop))block;
